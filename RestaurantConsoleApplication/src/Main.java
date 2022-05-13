27a35e8f04870c1adc28b3a11d78fcb52fd219a1
imvigneshkarthikeyan <imvigneshkarthikeyan@gmail.com>
2022-05-13 12:47:08 +0530

    Added SuperAdmin class

--- RestaurantConsoleApplication/src/Main.java
+++ RestaurantConsoleApplication/src/Main.java
@@ -1,10 +1,13 @@
-import static Utilities.UiUtils.*;
+// import static Utilities.UiUtils.*;
+// import static Utilities.ValidatorUtils.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // drawDoubleLine();
-        displayLineTitleAndUser("Welcome", "Super Admin");
-        displayLineWithTitle("Order History");
+        SuperAdmin superAdmin = new SuperAdmin();
+        superAdmin.loginAsSuperAdmin();
+        // displayLineTitleAndUser("Welcome", "Super Admin");
+        // displayLineWithTitle("Order History");
         // drawLine();
     }
 }
---
+++ RestaurantConsoleApplication/src/SuperAdmin.java
@@ -0,0 +1,31 @@
+import static Utilities.ValidatorUtils.*;
+
+public class SuperAdmin {
+    private final String SUPER_ADMIN_LOGIN_ID = "superadmin@abc.com";
+    private final String SUPER_ADMIN_LOGIN_PASS = "123";
+
+    public String getSuperAdminLogin() {
+		return SUPER_ADMIN_LOGIN_ID;
+	}
+
+    public String getSuperAdminPassword() {
+		return SUPER_ADMIN_LOGIN_PASS;
+	}
+
+    public void loginAsSuperAdmin() {
+        System.out.println("Enter Login ID: ");
+        String enteredLoginId = scanner.next();
+        if (enteredLoginId.equals(SUPER_ADMIN_LOGIN_ID)) {
+            System.out.println("ID is correct");
+            System.out.println("Enter the Login Password: ");
+            String enteredLoginPassword = readPassword();
+            if (enteredLoginPassword.equals(SUPER_ADMIN_LOGIN_PASS)) {
+                System.out.println("The password is correct....Entering into SuperAdminPanel");
+            } else {
+                System.out.println("Login password is wrong");
+            }
+        } else {
+            System.out.println("Login ID is wrong");
+        }
+    }
+}
