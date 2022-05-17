548a6887472efdb11dfebd49a45d48ba453f449d
imvigneshkarthikeyan <imvigneshkarthikeyan@gmail.com>
2022-05-17 10:34:23 +0530

    Added validation for admin login

--- RestaurantConsoleApplication/src/Admin/AdminAuthenticator.java
+++ RestaurantConsoleApplication/src/Admin/AdminAuthenticator.java
@@ -8,20 +8,47 @@ import Restaurant.RestaurantDatabase;
 
 public class AdminAuthenticator {
     public void authenticateAdmin(AdminDatabase adminDatabase, RestaurantDatabase restaurantDatabase, CustomerDatabase customerDatabase) {
-        drawDoubleLine();
-        System.out.println("Enter the Login Id: ");
-        String enteredID = scanner.next();
-        if (adminDatabase.getAdminMap().containsKey(enteredID)) {
-            drawLine();
-            System.out.println("ID is crt, now enter the password.");
-            String enteredPassword = readPassword();
-            if (adminDatabase.getAdminMap().get(enteredID).getLoginPassword().equals(enteredPassword)) {
+        int option = 1;
+        scanner.nextLine();
+        while (option == 1) {
+            drawDoubleLine();
+            System.out.println("Enter the Login Id: ");
+            String enteredID = scanner.nextLine();
+            if (adminDatabase.getAdminMap().containsKey(enteredID)==false) {
+                drawDoubleLine();
+                System.out.println("Login ID is wrong\nEnter 1: To try again\nEnter 2: Go Back");
+                option = scanner.nextInt();
+                scanner.nextLine();
+            } else {
                 drawLine();
-                System.out.println("Password is right");
-                displayLineTitleAndUser("Welcome", "Admin " + adminDatabase.getAdminMap().get(enteredID).getAdminName());
-                AdminFunctions adminFunctions = new AdminFunctions();
-                adminFunctions.executeAdminFunction(restaurantDatabase, customerDatabase);
+                System.out.println("ID is crt.");
+                drawLine();
+                authenticatePasswordForAdmin(enteredID, adminDatabase, restaurantDatabase, customerDatabase);
+                option = 2;
             }
         }
+        
+    }
+
+    public void authenticatePasswordForAdmin(String enteredID, AdminDatabase adminDatabase, RestaurantDatabase restaurantDatabase, CustomerDatabase customerDatabase) {
+        int option = 1;
+        while(option == 1) {
+            drawDoubleLine();
+            System.out.println("Enter Password: ");
+            String enteredPassword = readPassword();
+            if (adminDatabase.getAdminMap().get(enteredID).getLoginPassword().equals(enteredPassword)==false) {
+                System.out.println("Password is wrong\nEnter 1: To try again\nEnter 2: Go Back");
+                option = scanner.nextInt();
+                scanner.nextLine();
+            } else {
+                drawLine();
+                System.out.println("Password is right");
+                displayLineTitleAndUser("Welcome Admin ", adminDatabase.getAdminMap().get(enteredID).getAdminName());
+                AdminFunctions adminFunctions = new AdminFunctions();
+                adminFunctions.executeAdminFunction(restaurantDatabase, customerDatabase);
+                option = 2;
+            }
+        }
+        drawDoubleLine();
     }
 }
