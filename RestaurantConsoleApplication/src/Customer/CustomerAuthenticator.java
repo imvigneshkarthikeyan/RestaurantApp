d1025b3f608dd5bf2b2cbabdcbda7aab3afd2a1d
imvigneshkarthikeyan <imvigneshkarthikeyan@gmail.com>
2022-05-17 16:04:42 +0530

    Added option validation in customerAuthenticator

--- RestaurantConsoleApplication/src/Customer/CustomerAuthenticator.java
+++ RestaurantConsoleApplication/src/Customer/CustomerAuthenticator.java
@@ -14,21 +14,26 @@ public class CustomerAuthenticator {
         while (option == 1 || option == 2) {
             System.out.println("Enter 1: To Sign in \nEnter 2: New User? Sign Up \nEnter 3: Go Back");
             drawDoubleLine();
-            option = scanner.nextInt();
-            optionValidator(option, 1, 3);
-            switch (option) {
-                case 1:
-                    System.out.println("Signing in");
-                    customerFunctions.signIn(restaurantDatabase, customerDatabase, orderDatabase, restaurant);
-                    break;
-                case 2:
-                    customerFunctions.signUp(customerDatabase);
-                    break;
-                case 3:
-                    System.out.println("Going Back");
-                    break;
-                default:
-                    break;
+            try {
+                option = scanner.nextInt();
+                optionValidator(option, 1, 3);
+                switch (option) {
+                    case 1:
+                        System.out.println("Signing in");
+                        customerFunctions.signIn(restaurantDatabase, customerDatabase, orderDatabase, restaurant);
+                        break;
+                    case 2:
+                        customerFunctions.signUp(customerDatabase);
+                        break;
+                    case 3:
+                        System.out.println("Going Back");
+                        break;
+                    default:
+                        break;
+                }                
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid Input, please try again");
+                option = 1;
             }
         }
     }
