f6d9fb0672e753c4c6bd57f6644dab6fce0a91e6
imvigneshkarthikeyan <imvigneshkarthikeyan@gmail.com>
2022-05-17 11:50:07 +0530

    Added validation for Restaurant Login

--- RestaurantConsoleApplication/src/Restaurant/RestaurantAuthenticator.java
+++ RestaurantConsoleApplication/src/Restaurant/RestaurantAuthenticator.java
@@ -7,22 +7,45 @@ import Order.OrderDatabase;
 
 public class RestaurantAuthenticator {
     public void authenticateRestaurant(RestaurantDatabase restaurantDatabase, OrderDatabase orderDatabase) {
-        drawDoubleLine();
-        System.out.println("Enter the Login Id: ");
-        String enteredID = scanner.next();
-        drawLine();
-        for (Restaurant restaurant : restaurantDatabase.getRestaurantList()) {
-            if (restaurant.getLoginID().equals(enteredID)) {
-                System.out.println("ID is correct, enter password: ");
+        int option = 1;
+        while (option == 1) {
+            drawDoubleLine();
+            System.out.println("Enter the Login Id: ");
+            String enteredID = scanner.next();
+            drawLine();
+            for (Restaurant restaurant : restaurantDatabase.getRestaurantList()) {
+                if (restaurant.getLoginID().equals(enteredID)) {
+                    System.out.println("ID is correct");
+                    validatePasswordForRestaurant(restaurant, enteredID, restaurantDatabase, orderDatabase);
+                    option = 2;
+                    break;
+                } else {
+                    drawDoubleLine();
+                    System.out.println("Login ID is wrong\nEnter 1: To try again\nEnter 2: Go Back");
+                    option = scanner.nextInt();
+                    break;
+                }           
+            }
+        }
+    }
+
+    public void validatePasswordForRestaurant(Restaurant restaurant, String enteredID, RestaurantDatabase restaurantDatabase, OrderDatabase orderDatabase) {
+        int option = 1;
+        while (option == 1) {
+            System.out.println("Enter password: ");
+            String enteredPassword = readPassword();
+            drawLine();
+            if (restaurant.getLoginPassword().equals(enteredPassword)) {
                 drawLine();
-                String enteredPassword = readPassword();
-                if (restaurant.getLoginPassword().equals(enteredPassword)) {
-                    drawLine();
-                    System.out.println("Login Successful");
-                    displayLineTitleAndUser("Welcome", restaurant.getRestaurantName() + " Restaurant");
-                    RestaurantFunctions restaurantFunctions = new RestaurantFunctions();
-                    restaurantFunctions.executeRestaurantFunction(enteredID, restaurant, restaurantDatabase, orderDatabase);
-                }
+                System.out.println("Login Successful");
+                displayLineTitleAndUser("Welcome", restaurant.getRestaurantName() + " Restaurant");
+                RestaurantFunctions restaurantFunctions = new RestaurantFunctions();
+                restaurantFunctions.executeRestaurantFunction(enteredID, restaurant, restaurantDatabase, orderDatabase);
+                option = 2;
+            } else {
+                System.out.println("Password is wrong\nEnter 1: To try again\nEnter 2: Go Back");
+                option = scanner.nextInt();
+                scanner.nextLine();
             }
         }
     }
